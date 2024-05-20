d2f312857b2035629aa741781ebb6387e1c88dc2
habert <habert231@gmail.com>
2024-05-20 18:17:17 +0300

    authenticating users

--- route/urls.py
+++ route/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("register/", views.register, name="register"),
-    path("login/", views.login, name="login"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("dashboard/", views.dashboard, name="dashboard"),
 ]
--- route/views.py
+++ route/views.py
@@ -1,18 +1,69 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
+from django.db import IntegrityError
+from django.contrib.auth import authenticate, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import login as auth_login
 
-# acts as a helpers for logic
-from . import util
 
-# from django.http import HttpResponse
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password1"]
+        confirmation = request.POST["password2"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        if password != confirmation:
+            return render(
+                request, "cargo/register.html", {"message": "Passwords do not match"}
+            )
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(
+                request, "cargo/register.html", {"message": "Username already taken"}
+            )
+
+        # Log in the user after successful registration
+        auth_login(request, user)
+        return HttpResponseRedirect(reverse("dashboard"))
+    else:
+        return render(request, "cargo/register.html")
 
 
 def index(request):
     return render(request, "cargo/index.html")
 
 
-def register(request):
-    return render(request, "cargo/register.html")
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            auth_login(request, user)
+            return HttpResponseRedirect(reverse("dashboard"))
+        else:
+            return render(
+                request,
+                "cargo/login.html",
+                {"message": "Invalid username and/or password."},
+            )
+    else:
+        return render(request, "cargo/login.html")
 
 
-def login(request):
+def dashboard(request):
+    return render(request, "cargo/dashboard.html")
+
+
+def logout_view(request):
     return render(request, "cargo/login.html")
